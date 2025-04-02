138d9eedf14bbb9b2c261ca3492afa17d4869a34
José Campillo <jcampillo1207@gmail.com>
2025-04-02 15:50:54 -0600

    Changes

--- components/universal/header.tsx
+++ components/universal/header.tsx
@@ -1,7 +1,16 @@
 import Link from "next/link";
 import { Logo } from "../svgs/logo";
 import { Button } from "../ui/button";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ChevronRight, Menu, X } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 
 export const Header = () => {
   return (
@@ -9,7 +18,7 @@ export const Header = () => {
       <Link href="/" className="h-full w-auto">
         <Logo />
       </Link>
-      <div className="w-fit h-full flex gap-x-1 items-center justify-end">
+      <div className="w-fit h-full hidden gap-x-1 items-center justify-end lg:flex">
         <Button
           asChild
           variant={"ghost"}
@@ -44,6 +53,81 @@ export const Header = () => {
             Contáctanos <ArrowRight />
           </Link>
         </Button>
+      </div>
+      <div className="w-fit h-full flex gap-x-1 items-center justify-end lg:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"right"} className="p-0 [&>button]:hidden w-full">
+            <div className="flex h-full w-full flex-col">
+              <SheetHeader className="bg-muted border-b p-3 h-14 text-start">
+                <div className="w-full h-full justify-between flex gap-x-5 items-center">
+                  <Link href="/" className="h-full w-auto">
+                    <Logo />
+                  </Link>
+                  <SheetClose asChild>
+                    <Button variant={"outline"} size={"icon"}>
+                      <X />
+                    </Button>
+                  </SheetClose>
+                </div>
+              </SheetHeader>
+              <div className="flex min-h-0 flex-1 flex-col gap-2 overflow-auto p-3">
+                <SheetClose asChild>
+                  <Button
+                    asChild
+                    variant={"ghost"}
+                    size={"lg"}
+                    className="font-medium justify-between px-3"
+                  >
+                    <Link href={"/about"}>
+                      Nosotros <ChevronRight />
+                    </Link>
+                  </Button>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Button
+                    asChild
+                    variant={"ghost"}
+                    size={"lg"}
+                    className="font-medium justify-between px-3"
+                  >
+                    <Link href={"/services"}>
+                      Servicios <ChevronRight />
+                    </Link>
+                  </Button>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Button
+                    asChild
+                    variant={"ghost"}
+                    size={"lg"}
+                    className="font-medium justify-between px-3"
+                  >
+                    <Link href={"/portafolio"}>
+                      Portafolio <ChevronRight />
+                    </Link>
+                  </Button>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Button
+                    asChild
+                    variant={"default"}
+                    size={"lg"}
+                    className="font-medium justify-between px-3"
+                  >
+                    <Link href={"/portafolio"}>
+                      Contáctanos <ArrowRight />
+                    </Link>
+                  </Button>
+                </SheetClose>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
